b1db61d6c3f6454bda616d5aa2e4eb51c35ecfa6
chen.zhang <zhch0633@live.cn>
2015-01-21 15:37:38 +0800

    basic popUpWindows :star2:
    use popupWindows to host buttons

--- MyApplication/app/src/main/java/com/example/myapplication/DemoActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/DemoActivity.java
@@ -1,17 +1,22 @@
 package com.example.myapplication;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsoluteLayout;
 import android.widget.Button;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 
 public class DemoActivity extends ActionBarActivity implements View.OnClickListener {
 
-    Button testButton;
+    private Button testButton;
+    private PopupWindow mPopupWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,13 @@ public class DemoActivity extends ActionBarActivity implements View.OnClickListe
         setContentView(R.layout.activity_demo);
         testButton = (Button)findViewById(R.id.button);
         testButton.setOnClickListener(this);
+
+        View popupView = getLayoutInflater().inflate(R.layout.popup_window, null);
+
+        mPopupWindow = new PopupWindow(popupView, AbsoluteLayout.LayoutParams.WRAP_CONTENT, AbsoluteLayout.LayoutParams.WRAP_CONTENT, true);
+        mPopupWindow.setTouchable(true);
+        mPopupWindow.setOutsideTouchable(true);
+        mPopupWindow.setBackgroundDrawable(new BitmapDrawable(getResources(), (Bitmap) null));
     }
 
     @Override
@@ -57,5 +69,6 @@ public class DemoActivity extends ActionBarActivity implements View.OnClickListe
 
     public void windowPopUp(View v){
         ((TextView)findViewById(R.id.textView)).setText("calling popup");
+        mPopupWindow.showAsDropDown(v);
     }
 }
