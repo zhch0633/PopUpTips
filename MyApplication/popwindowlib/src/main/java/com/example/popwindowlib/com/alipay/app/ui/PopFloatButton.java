20b00b3707f3f9a9dd7f98a96d480da7233a98a6
chen.zhang <zhch0633@live.cn>
2015-01-26 11:35:11 +0800

    test 2 :100:

--- MyApplication/popwindowlib/src/main/java/com/example/popwindowlib/com/alipay/app/ui/PopFloatButton.java
+++ MyApplication/popwindowlib/src/main/java/com/example/popwindowlib/com/alipay/app/ui/PopFloatButton.java
@@ -14,15 +14,19 @@ import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
  * this will be used to contruct a new round view form popupwindow that can drag and click
  */
 public class PopFloatButton extends PopDragableBase{
+    private RoundImageView mRoundImageView;
+
     public PopFloatButton(View view, int w,int h,boolean focusable){
         super(view,w,h,focusable);
+        mRoundImageView = (RoundImageView)view.findViewById(R.id.dragacher);
         popupView = view;
     }
 
     public PopFloatButton(Activity activity){
-        super(activity.getLayoutInflater().inflate(R.layout.round_pop_layout, null), WRAP_CONTENT, WRAP_CONTENT,true);
+        super(activity.getLayoutInflater().inflate(R.layout.round_pop_layout, null), WRAP_CONTENT, WRAP_CONTENT,false);
+        mRoundImageView = (RoundImageView)popupView.findViewById(R.id.dragacher);
         setTouchable(true);
-        setOutsideTouchable(true);
+        setOutsideTouchable(false);
         setBackgroundDrawable(new BitmapDrawable(activity.getResources(), (Bitmap) null));
         setDragable(true);
     }
